2dfc3b6e47577e43363cedb23740c436fc46e1e7
ammo-stash <120314284+ammo-stash@users.noreply.github.com>
2023-02-13 10:31:33 +0100

    Add files via upload

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,3 +1,3 @@
 python.exe -m pip install --upgrade pip
 pip install --upgrade setuptools
-pip install mss numpy pywin32 opencv-python customtkinter pandas ipython psutil pyyaml tqdm matplotlib seaborn gitpython scipy wheel
+pip install mss numpy pywin32 opencv-python customtkinter pandas ipython psutil pyyaml tqdm matplotlib seaborn gitpython scipy wheel keyboard
--- main.py
+++ main.py
@@ -1,49 +1,103 @@
+import argparse
+import time
+import customtkinter as ctk
+import cv2
+import keyboard
 import mss
 import numpy as np
 import torch
 import win32api
 import win32con
-import cv2
-import ctypes
-import customtkinter as ctk
 from PIL import Image, ImageTk
+
+parser = argparse.ArgumentParser()
+parser.add_argument("game", help="Select game", type=int)
+args = parser.parse_args()
+game = args.game / 10
+if args.game == 44:
+    trigger = 10
+else:
+    trigger = 2
+
+width = win32api.GetSystemMetrics(win32con.SM_CXSCREEN) / 2
+height = win32api.GetSystemMetrics(win32con.SM_CYSCREEN) / 2
+start_time = time.time()
+wconst = 640
+frame_count = 0
+fps = 0
+scale = 1
+key_pressed1 = False
+key_pressed2 = False
+half = wconst / 2
+screen = mss.mss()
+detection_box = {'left': int(width - (wconst / 2)), 'top': int(height - (wconst / 2)),
+                 'width': int(wconst), 'height': int(wconst)}
 
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("dark-blue")
 root = ctk.CTk()
-root.geometry("640x640")
+root.geometry("1000x1000")
 root.title("Options")
-frame = ctk.CTkFrame(master=root)
+root.resizable(False, False)
 
-width = ctypes.windll.user32.GetSystemMetrics(0) / 2
-height = ctypes.windll.user32.GetSystemMetrics(1) / 2
-window_constant = 250
-scale = 2
+var1 = ctk.IntVar()
+checkbox1 = ctk.CTkCheckBox(master=root, text="Softaim", variable=var1)
+checkbox1.place(x=10, y=10)
 
-screen = mss.mss()
-detection_box = {'left': int(width - (window_constant / 2)), 'top': int(height - (window_constant / 2)),
-                 'width': int(window_constant), 'height': int(window_constant)}
+var2 = ctk.IntVar()
+checkbox2 = ctk.CTkCheckBox(master=root, text="Triggerbot", variable=var2)
+checkbox2.place(x=10, y=35)
+
+img = ctk.CTkLabel(root, image=None, text="")
+img.place(x=360, y=360)
+
+label = ctk.CTkLabel(root, text=f"FPS: {fps}", font=("Arial", 40))
+label.place(x=360, y=300)
+
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
 model.conf = 0.75
 
-checkbox1 = ctk.CTkCheckBox(master=root, text="Softaim")
-checkbox1.pack(ipadx=310, pady=10)
 
-checkbox2 = ctk.CTkCheckBox(master=root, text="Triggerbot")
-checkbox2.pack(ipadx=310)
+def keybinds():
+    global key_pressed1, key_pressed2
+    if keyboard.is_pressed('&') and not key_pressed1:
+        checkbox1.toggle()
+        key_pressed1 = True
+    elif not keyboard.is_pressed('&'):
+        key_pressed1 = False
+    if keyboard.is_pressed('é') and not key_pressed2:
+        checkbox2.toggle()
+        key_pressed2 = True
+    elif not keyboard.is_pressed('é'):
+        key_pressed2 = False
 
-img0 = None
-img = ctk.CTkLabel(root, image=img0, text="")
-img.pack()
+
+def window(window0):
+    img0 = cv2.cvtColor(window0, cv2.COLOR_BGR2RGB)
+    img0 = cv2.resize(img0, (640, 640), interpolation=cv2.INTER_AREA)
+    img0 = ImageTk.PhotoImage(Image.fromarray(img0))
+    img.configure(image=img0)
+    label.configure(text=f"FPS: {round(fps)}")
+
 
 while True:
     root.update()
+    keybinds()
+    # noinspection PyTypeChecker
+    frame = np.array(screen.grab(detection_box))
+    window(frame)
+    start_time = time.time()
+    frame_count = 0
+    fps = 0
+
     while checkbox1.get() == 1:
         root.update()
-
-        if cv2.waitKey(250) & 0xFF == ord('0'):
-            break
-
+        keybinds()
+        frame_count += 1
+        elapsed_time = time.time() - start_time
+        if elapsed_time != 0:
+            fps = frame_count / elapsed_time
+        # noinspection PyTypeChecker
         frame = np.array(screen.grab(detection_box))
         results = model(frame)
 
@@ -53,23 +107,22 @@ while True:
                 x1 = int(box[0])
                 y2 = int(box[3])
                 y1 = int(box[1])
-                x = int(((x1 + x2) / 2) - (window_constant / 2))
-                y = int(((y1 + y2) / 2) - ((y2 - y1) / 2.2) - (window_constant / 2))
+                x = int(((x1 + x2) / 2) - half)
+                y = int(((y1 + y2) / 2) - ((y2 - y1) / game) - half)
+                own_player = x1 < wconst / 2.7 < y2
 
-                if win32api.GetKeyState(0x02) in (-127, -128) and not ((window_constant / 2) - 1 <= x + window_constant / 2 <= (window_constant / 2) + 1 and (window_constant / 2) - 1 <= y + window_constant / 2 <= (window_constant / 2) + 1):
+                if win32api.GetKeyState(0x02) in (-127, -128) and not own_player \
+                        and not (half - 1 <= x + half <= half + 1 and half - 1 <= y + half <= half + 1):
                     win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(x * scale), int(y * scale), 0, 0)
 
-                if win32api.GetKeyState(0x02) in (-127, -128) and (window_constant / 2) - 6 <= x + window_constant / 2 <= (window_constant / 2) + 6 and (
-                        window_constant / 2) - 6 <= y + window_constant / 2 <= (
-                        window_constant / 2) + 6 and checkbox2.get() == 1:
+                if win32api.GetKeyState(0x02) in (-127, -128) and not own_player \
+                        and half - trigger <= x + half <= half + trigger \
+                        and half - trigger <= y + half <= half + trigger \
+                        and checkbox2.get() == 1:
                     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
                     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-
-            cv2.circle(frame, (int(x + (window_constant / 2)), int(y + (window_constant / 2))), 2, (0, 255, 0), -1)
-            cv2.line(frame, (int(window_constant / 2), int(window_constant / 2)),
-                     (x + int(window_constant / 2), y + int(window_constant / 2)), (0, 0, 255), 1)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 1)
-        img0 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img0 = cv2.resize(img0, (440, 440), interpolation=cv2.INTER_AREA)
-        img0 = ImageTk.PhotoImage(Image.fromarray(img0))
-        img.configure(image=img0)
+                cv2.circle(frame, (int(x + half), int(y + half)), 2, (0, 255, 0), -1)
+                cv2.line(frame, (int(half), int(half)),
+                         (x + int(half), y + int(half)), (0, 0, 255), 1)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 1)
+        window(frame)
