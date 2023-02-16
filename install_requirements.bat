fb4a72f73d6d22d73814e2cd730b344c8cf56b8b
ammo-stash <120314284+ammo-stash@users.noreply.github.com>
2023-02-16 21:00:57 +0100

    Delete install_requirements.bat

--- install_requirements.bat
+++
@@ -1,3 +0,0 @@
-python.exe -m pip install --upgrade pip
-pip install --upgrade setuptools
-pip install mss numpy pywin32 opencv-python customtkinter pandas ipython psutil pyyaml tqdm matplotlib seaborn gitpython scipy wheel keyboard
